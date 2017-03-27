3018962c2d39a02443a585ce1910266c6531be14
KAU <KAU@KAU-PC>
2017-03-27 17:51:57 +0900

    KAU 2014122067 / 2017-03-27 Java / Lab4

--- Account(1)/src/CheckingAccount.java
+++ Account(1)/src/CheckingAccount.java
@@ -20,6 +20,7 @@ public class CheckingAccount extends Account {
 			System.out.println("출금 한도를 넘었습니다. 출금 한도 : " + creditLimit);
 		} else {
 			setBalance(getBalance() - debit);
+			creditLimit = creditLimit - debit;
 			
 			if (getBalance() < 0) {
 				System.out.println("마이너스 입니다!");
@@ -38,5 +39,20 @@ public class CheckingAccount extends Account {
 			setBalance(bal);
 		} 	
 	}
-
+	
+	public double getWithdrawableAccount() {
+		return creditLimit;
+	}
+	
+	public void passTime(int time) {
+		double bal = getBalance();
+		
+		creditLimit = creditLimit + bal * Math.pow(loanInterest, time);
+		bal = bal + bal * Math.pow(loanInterest, time);
+		setBalance(bal);
+		if (creditLimit < 0) {
+			System.out.println("accout1 went Bankrupt");
+		}
+	}
 }
+
