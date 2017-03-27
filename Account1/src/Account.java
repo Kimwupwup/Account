535c2fd148f8cad6e4c221a49d88cf4b791ef293
KAU <KAU@KAU-PC>
2017-03-27 19:53:34 +0900

    ...

---
+++ Account1/src/Account.java
@@ -0,0 +1,32 @@
+
+public class Account {
+	
+	private double balance;	//계좌
+
+	
+	Account(double balance) {
+		this.balance = balance;
+	}
+	
+	public void credit(double credit) {	//계좌에 입금
+		balance = balance + credit;
+	}
+	
+	protected void setBalance(double balance) {	//balance setter
+		this.balance = balance;
+	}
+	
+	public double getBalance() {	//balance getter
+		return balance;
+	}
+	
+	public void debit(double debit) {	//계좌 출금
+		
+		if(balance - debit > 0) {	//계좌1에 잔고확인 후 작업
+			balance = balance - debit;
+		} else {
+			System.out.println("Debit amount exceed account balance.");
+		}
+	}
+
+}
---
+++ Account1/src/AccountTest.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class AccountTest {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		CheckingAccount chkAccount1 = new CheckingAccount(100.0, 200.0, 0.1, 0.2);
+		CheckingAccount chkAccount2 = new CheckingAccount(100.0, 200.0, 0.1, 0.2);
+		
+		//출력
+		System.out.println("Account1 balance : $" + chkAccount1.getBalance());
+		System.out.println("Account2 balance : $" + chkAccount2.getBalance());
+		
+		
+		//돈입력 후 값들 Account에 저장
+		System.out.print("Enter deposit amount for Account1 : $");
+		double acc1 = scanner.nextDouble();
+		chkAccount1.credit(acc1);
+		System.out.print("Enter deposit amount for Account2 : $");
+		double acc2 = scanner.nextDouble();
+		chkAccount2.credit(acc2);
+		
+		
+		
+		//출력
+		System.out.println("Account1 balance : $" + chkAccount1.getBalance());
+		System.out.println("Account2 balance : $" + chkAccount2.getBalance());
+		
+		
+		
+		//출금
+		System.out.print("Enter withdrawal amount for Account1 : $");
+		double debit = scanner.nextDouble();
+		System.out.println("Subtracting $" + debit + " from Account1");
+		chkAccount1.debit(debit);
+
+		
+		System.out.println("Account1 balance : $" + chkAccount1.getBalance());
+		System.out.println("Account2 balance : $" + chkAccount2.getBalance());
+		
+		
+		System.out.print("Enter withdrawal amount for Account2 : $");
+		debit = scanner.nextDouble();
+		System.out.println("Subtracting $" + debit + " from Account2");
+		chkAccount2.debit(debit);
+		
+		System.out.println("Account1 balance : $" + chkAccount1.getBalance());
+		System.out.println("Account2 balance : $" + chkAccount2.getBalance());
+		
+		
+		
+		//이자계산
+		chkAccount1.nextMonth();
+		chkAccount2.nextMonth();
+		
+		System.out.println("Next Month!");
+		System.out.println("Account1 balance : $" + chkAccount1.getBalance());
+		System.out.println("Account1 balance : $" + chkAccount2.getBalance());
+		
+	}
+}	
