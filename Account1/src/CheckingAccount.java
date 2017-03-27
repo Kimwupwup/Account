2cdbd41923f19e6306dd47c952fcff5074c05a36
wndud <wndud@Wup>
2017-03-27 20:29:27 +0900

    KAU 2014122067 / 2017-03-20 Java / Lab3

--- Account1/src/CheckingAccount.java
+++ Account1/src/CheckingAccount.java
@@ -4,7 +4,6 @@ public class CheckingAccount extends Account {
 	private double creditLimit;
 	private double interest;
 	private double loanInterest;
-	private double bal;
 	
 	CheckingAccount(double balance, double creditLimit, double interest, double loanInterest) {
 		super(balance);
@@ -16,26 +15,24 @@ public class CheckingAccount extends Account {
 
 	@Override
 	public void debit(double debit) {
-		bal = getBalance();
-		
 		if (debit > creditLimit) {
 			System.out.println("출금 한도를 넘었습니다. 출금 한도 : " + creditLimit);
-		} else {
-			bal = bal - debit;
+		} else {			
+			setBalance(getBalance() - debit);
 			
-			setBalance(bal);
-			
-			if (bal < 0) {
+			if (getBalance() < 0) {
 				System.out.println("마이너스 입니다!");
 			}
 		}
 	}
 	
 	public void nextMonth() {
-		if (bal > 0) {
+		if (getBalance() > 0) {
+			double bal =getBalance();
 			bal = bal + bal * interest;
 			setBalance(bal);
-		} else if (bal < 0) {
+		} else if (getBalance() < 0) {
+			double bal = getBalance();
 			bal = bal + bal * loanInterest;
 			setBalance(bal);
 		} 	
