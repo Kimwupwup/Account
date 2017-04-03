e10b5ffa9a6f4a443da7a4db881dcd304e34fcca
Kimwupwup <wndud1640@naver.com>
2017-04-03 17:15:08 +0900

    KAU 2014122067 / 2017-04-03 Java Account3 ValuableTest 수정 객체형성 간략하게

--- Account3/src/ValuableTest.java
+++ Account3/src/ValuableTest.java
@@ -3,17 +3,11 @@ public class ValuableTest {
 
 	public static void main(String[] args) {
 				
-		Account[] account = new Account[2];
-		account[0] = new CheckingAccount(100.0, 50.0, 0.01, 0.07);
-		account[1] = new SavingsAccount(100.0, 0.05);
-		Human human = new Human("Mark", 20);
-		Car car = new Car("MyCar", 5000);
-		
 		Valuable[] objectList = new Valuable[4];
-		objectList[0] = account[0];
-		objectList[1] = account[1];
-		objectList[2] = human;
-		objectList[3] = car;
+		objectList[0] = new CheckingAccount(100.0, 50.0, 0.01, 0.07);
+		objectList[1] = new SavingsAccount(100.0, 0.05);
+		objectList[2] = new Human("Mark", 20);
+		objectList[3] = new Car("MyCar", 5000);
 		
 		
 		for(Valuable objectItem : objectList) {
