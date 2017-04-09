cdabb9bb022b0fba77a9afeb5641a489889c10fd
Kimwupwup <wndud1640@naver.com>
2017-04-10 02:04:54 +0900

    KAU 2014122067 / 2017-04-10 Java Account3 ValueableTest for문 설명추가
    for(자료형 자료형이름 : 배열이름) - objectList에 있는 것을 순서대로 objectItem에 넣어줌

--- Account3/src/ValuableTest.java
+++ Account3/src/ValuableTest.java
@@ -10,8 +10,8 @@ public class ValuableTest {
 		objectList[3] = new Car("MyCar", 5000);
 		
 		
-		for(Valuable objectItem : objectList) {
-			System.out.printf("%s \n", objectItem.toString());
+		for(Valuable objectItem : objectList) {		//for(자료형 자료형이름 : 배열이름) - objectList에 있는 것을 순서대로 objectItem에 넣어줌
+			System.out.printf("%s \n", objectItem.toString());	//objectItem에 들어잇는 objectList를 참조하는 것
 			System.out.printf("6 month later Valuable : %.2f\n", objectItem.EstimateValue(6));
 			System.out.println();
 		}
